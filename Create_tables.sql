c8dcd3a4d679aa9063cd0f6cb32f97712e41a229
MOMIV <i.v.momotova@mail.ru>
2023-09-20 23:08:19 +0500

    chande scropt, add scheme

--- Create_tables.sql
+++ Create_tables.sql
@@ -26,9 +26,20 @@ create table if not exists  issued_books(
 	index_number serial PRIMARY KEY,
 	reader_id bigint,
 	book_id VARCHAR ( 255 ),
-	year_of_publishing date NOT NULL,
 	date_issue date,
 	date_return date,
 	FOREIGN KEY(reader_id) REFERENCES readers(reader_id) ON DELETE CASCADE,
 	FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE
 );
+
+INSERT INTO readers (readername, adress, phone) 
+VALUES 
+('Ivanov Ivan Ivanovich', 'Moskow, Novaya street 45-63', '+79457894512'),
+('Petrov Petr Andreevich', 'Rostov, Svetlaya  str 4', '89178899663'),
+('Ivanova Valentina Petrovys', 'Moskow, GnezdoKykyshki 77', '+74958899777');
+
+INSERT INTO publishing_houses (publishing_house_name, city) 
+VALUES 
+('NewView', 'Moskow'),
+('Oko', 'Magadan');
+
